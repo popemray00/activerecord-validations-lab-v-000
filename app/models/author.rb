667cf85cab5c72bebcaa31b20af19113289a4b90
Michael Pope <popemray1986@gmail.com>
2018-07-17 03:07:34 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :phone_number, length: {is: 10}
-  validates :author, uniqueness: true
+  validates :name, uniqueness: true
 end
