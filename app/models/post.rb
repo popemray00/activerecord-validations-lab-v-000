06fe21916b3d8d8970e779407ecc4f9f94a7fc12
Michael Pope <popemray1986@gmail.com>
2018-07-17 03:38:08 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: {minimum: 8,  :too_short '%{count}'}
+  validates :content, length: {minimum: 250}
 
 
 end
