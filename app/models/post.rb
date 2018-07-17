8c9f0aa6e9cdc23f15be28222635a12ab37d0efd
Michael Pope <popemray1986@gmail.com>
2018-07-17 03:19:38 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: {minimum: 2}
-  validates :content, length: {maximum: 20}
+  validates :content, length: {is: 20}
 
 
 end
